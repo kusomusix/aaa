471fd7e81a7dd7a3a00bca0f4f46c9da928c0014
kusomusix <kusomusix@sonicep.local>
2017-11-26 18:33:38 +0900

    no message

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,60 @@
 var gulp = require('gulp');
+var path = require('path');
 var sass = require('gulp-sass');
+var connect = require('gulp-connect');
+var plumber = require('gulp-plumber'); //監視中のエラー防止
+var notify  = require('gulp-notify'); //デスクトップ通知
 //var browserSync = require('browser-sync').create();
 
-gulp.task('sass', function() {
-  return gulp.src([
-    '_source/scss/*.scss'
-  ])
-  .pipe(sass().on('error', sass.logError))
-  .pipe(gulp.dest('html'))
-//  .pipe(browserSync.stream());
+//option
+var paths = {
+  srcDir : '_source',
+  dstDir : 'dist'
+}
+
+//sass
+gulp.task('sass', function(){
+  var srcGlob = path.join( paths.srcDir, '_scss', '*.scss' );
+  var dstGlob = path.join( paths.dstDir, 'css' )
+  var errorMessage = "Error: <%= error.message %>";
+
+	gulp.src( srcGlob )
+//  gulp.src('./_source/_scss/*.scss') //タスクで処理するソースの指定
+    .pipe(plumber({
+      errorHandler: notify.onError( errorMessage )
+    }))
+    .pipe(sass()) //処理させるモジュールを指定
+    .pipe(gulp.dest(dstGlob)); //保存先を指定
+//    .pipe(connect.reload());
 });
 
-gulp.task('copy', function() {
-  return gulp.src([
-    '_source/scss/*',
-    '!_source/scss/*.scss'
-  ])
-  .pipe(gulp.dest('html'))
-//  .pipe(browserSync.stream());
+//copy
+gulp.task('copy', function(){
+  var srcGlob = paths.srcDir + '/*.html';
+  var dstGlob = paths.dstDir;
+
+  gulp.src(srcGlob)
+    .pipe(gulp.dest(dstGlob));
+//    .pipe(connect.reload());
 });
 
-gulp.task('watch', function() {
-  // browserSync.init({
-  //   server: {
-  //     baseDir: "html"
-  //   }
-  // });
+//watch
+//gulp.task('watch', function(){
+gulp.task('watch', ['sass', 'copy'], function(){
+  var scssWatchGlob = paths.srcDir + '_scss/*.scss';
+  var htmlWatchGlob = paths.srcDir + '/*.html';
 
-  gulp.watch(['_source/scss/*.scss'], ['sass']);
-  gulp.watch([
-    '_source/scss/*',
-    '!_source/scss/*.scss'
-    ], ['copy']);
+  gulp.watch(scssWatchGlob, ['sass']);
+  gulp.watch(htmlWatchGlob, ['copy']);
 });
 
-gulp.task('default', ['copy', 'sass', 'watch']);
+//server
+gulp.task('serve', function(){
+  connect.server({
+    root: paths.dstDir,
+//    livereload: true
+  });
+});
 
+//gulp.task('default', ['sass']);
+gulp.task('default', ['watch', 'serve']);
